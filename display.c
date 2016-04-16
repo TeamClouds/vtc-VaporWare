bf0c4094d63de2d91d96cfad702d5e140a92277b
Stephen Bird <sebirdman@gmail.com>
2016-04-16 00:59:33 -0700

    Make Vaping Vape again
    - Loops
    - Button presses
    - More

--- display.c
+++ display.c
@@ -25,14 +25,54 @@
 #include <Atomizer.h>
 #include <TimerUtils.h>
 #include <Battery.h>
-
+#include <Button.h>
+#include "main.h"
+extern struct globals g;
 void sleepDisplay(uint32_t counterIndex) {
 	Display_SetOn(0);
 }
 
-void updateScreen(volatile Atomizer_Info_t atomInfo, volatile uint32_t watts) {
-	const char *atomState;
-	char buf[100];
+
+inline void getMenuDumbText(char *buff) {
+    siprintf(buff, "Herro");
+}
+
+void showMenu() {
+    char buff[8];
+	Display_Clear();
+    getMenuDumbText(buff);
+	Display_PutText(0, 60, buff, FONT_DEJAVU_8PT);
+	Display_Update();
+}
+
+
+inline void getTemperature(char *buff, uint32_t temperature) {
+    siprintf(buff, "%lu", temperature);
+}
+
+inline void getPercent(char *buff, uint8_t percent) {
+    siprintf(buff, "%d%%", percent);
+}
+
+inline void getState(char *buff, char *state, uint8_t intstate) {
+    siprintf(buff, "%s(%i)", state, intstate);
+}
+
+inline void getResistance(char *buff, uint32_t resistance) {
+	siprintf(buff, "%3lu.%luO",
+		resistance / 1000,
+		resistance % 1000 / 10);
+}
+
+inline void getWatts(char *buff, uint32_t watts) {
+	siprintf(buff, "%3lu.%luW",
+		watts / 1000,
+		watts % 1000 / 100);
+}
+
+
+void updateScreen(struct globals *g) {
+	char *atomState;
 	uint16_t battVolts;
     uint8_t battPerc;
 
@@ -57,7 +97,7 @@ void updateScreen(volatile Atomizer_Info_t atomInfo, volatile uint32_t watts) {
 			atomState = "TOO HOT";
 			break;
 		default:
-			if (atomInfo.temperature >= 600) {
+			if (g->atomInfo.temperature >= 600) {
         	    atomState = "PROTECT";
         	} else {
         		atomState = Atomizer_IsOn() ? "FIRING" : "";
@@ -65,21 +105,41 @@ void updateScreen(volatile Atomizer_Info_t atomInfo, volatile uint32_t watts) {
 			break;
 	}
 
-	siprintf(buf, "%3lu.%luW\n%2d.%02do\n%2d.%02dA\n%5luF\n%s\n\n\n\n%d%%\n%s",
-		watts / 1000,
-		watts % 1000 / 100,
-		atomInfo.resistance / 1000,
-		atomInfo.resistance % 1000 / 10,
-		atomInfo.current / 1000,
-		atomInfo.current % 1000 / 10,
-		atomInfo.temperature,
-		atomState,
-		battPerc,
-		Battery_IsCharging() & Battery_IsPresent() ? "CHARGING" : "");
-
+	//Battery_IsCharging() & Battery_IsPresent() ? "CHARGING" : ""
 	Display_Clear();
-	Display_PutText(0, 0, buf, FONT_DEJAVU_8PT);
+
+    char buff[8];
+    getTemperature(buff, g->atomInfo.temperature);
+	Display_PutText(0, 0, buff, FONT_DEJAVU_8PT);
+
+    getTemperature(buff, g->atomInfo.base_temperature);
+	Display_PutText(0, 10, buff, FONT_DEJAVU_8PT);
+
+    getWatts(buff, g->watts);
+	Display_PutText(0, 20, buff, FONT_DEJAVU_8PT);
+
+    getResistance(buff, g->atomInfo.resistance);
+	Display_PutText(0, 30, buff, FONT_DEJAVU_8PT);
+
+	getResistance(buff, g->atomInfo.base_resistance);
+    Display_PutText(0, 40, buff, FONT_DEJAVU_8PT);
+
+// NEVER FUCKING CALL FUNCTIONS HERE YOU FUCKING FUCK
+//    getState(buff, atomState, Button_GetState());
+//	Display_PutText(0, 50, buff, FONT_DEJAVU_8PT);
+
+    getPercent(buff, battPerc);
+	Display_PutText(0, 60, buff, FONT_DEJAVU_8PT);
+
+    getTemperature(buff, g->vapeCnt);
+	Display_PutText(0, 70, buff, FONT_DEJAVU_8PT);
+
+    getTemperature(buff, g->whatever);
+	Display_PutText(0, 80, buff, FONT_DEJAVU_8PT);
+
 	Display_Update();
 
-	Timer_CreateTimeout(1000, 0, sleepDisplay, 0);
+	//Timer_CreateTimeout(1000, 0, sleepDisplay, 0);
 }
+
+
