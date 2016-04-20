9c0a5c6ff92f325f5a302012a952ccef81f869e9
Roman Birg <romanbirg@gmail.com>
2016-04-20 14:25:39 -0700

    genfont: imrpove parsing
    Previously if output ended in the middle of a line (namely bracket was
    not in its own line), we'd end up cutting out the last line.
    
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- tools/genfont.sh
+++ tools/genfont.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-if [[ $# -eq 0 ]]
-  then
+if [ $# -eq 0 ] ;  then
     echo "No arguments supplied;, usage: genfont.sh <location of font> <output point size>"
 fi
 
-if [[ -z $LIST ]] ; then
+if [ -z $LIST ] ; then
     echo "defaulting to 0-9 font list"
     LIST=`seq 0 9`
 fi
@@ -16,9 +15,9 @@ SIZE=$2
 echo "bitmap data\n\n"
 
 for i in $LIST; do
-    echo "  //$i"
+    echo "//$i"
     # at the end, we remove the last 4 bytes to clear out the extra curly brace
-    convert -font $FONT -pointsize $SIZE -rotate 270 -flip -endian LSB label:$i xbm:- | grep '0x\w\w' | head -c-4
+    convert -font $FONT -pointsize $SIZE -rotate 270 -flip -endian LSB label:$i xbm:- | grep -o '0x\w\w' | awk '{ if (NR%8==0) printf $1 ", \n"; else printf $1 ", "; }'
     echo "\n";
 done
 
