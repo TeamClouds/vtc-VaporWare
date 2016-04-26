385605f2732036dcf36607484c71a10c8e591455
Roman Birg <romanbirg@gmail.com>
2016-04-26 12:22:57 -0700

    genfont: generate whole font file by default
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- tools/genfont.sh
+++ tools/genfont.sh
@@ -4,15 +4,40 @@ if [ $# -eq 0 ] ;  then
     echo "No arguments supplied;, usage: genfont.sh <location of font> <output point size>"
 fi
 
+BIGFONT=false
+
 if [ -z $LIST ] ; then
-    echo "defaulting to 0-9 font list"
     LIST=`seq 0 9`
+    BIGFONT=true
 fi
 
 FONT=$1
 SIZE=$2
 
-echo "bitmap data\n\n"
+if [ $BIGFONT == "true" ] ; then
+    cat <<EOF
+/********************************
+ * adapted from font_sun12x22.c *
+ * by Jurriaan Kalkman 06-2005  *
+ ********************************/
+
+#include <stdint.h>
+#include <Font_Data.h>
+
+// There was much suffering to make something that works right
+// Hours and hours of pain and suffering
+//
+//
+// How to make:
+// convert -font Dejavu-Sans-Mono -pointsize 24 -rotate 270 -flip -endian LSB label:4 4.xbm
+// In the file that outputs, flip the height and width vars that it tells you.
+// see \$EVICSDK/tools/genfont.sh
+
+const uint8_t fontdata_10x18[] = {
+EOF
+else
+    echo "bitmap data\n\n"
+fi
 
 for i in $LIST; do
     echo "//$i"
@@ -21,7 +46,13 @@ for i in $LIST; do
     echo "\n";
 done
 
-echo "size table\n\n"
+if [ $BIGFONT == "true" ] ; then
+    echo "};\n"
+    echo "const Font_CharInfo_t fontdata_10x18_Descriptors[] = {\n"
+else
+    echo "size table\n\n"
+fi
+
 COUNT=0
 for i in $LIST; do
     WIDTH=`convert -font $FONT -pointsize $SIZE -rotate 270 -flip -endian LSB label:$i xbm:- | grep "height" | cut -d ' ' -f3`
@@ -31,3 +62,18 @@ for i in $LIST; do
     echo "{$WIDTH, $COUNT}, /* $i w: $WIDTH h: $HEIGHT */"
     COUNT=$(( $COUNT + $BYTESIZE ))
 done
+
+if [ $BIGFONT == "true" ] ; then
+    cat <<EOF
+};
+
+const Font_Info_t fontdata_10x18_fontinfo = {
+    $HEIGHT, /* Character height */
+    '0', /* Start character */
+    '9', /* End character */
+    15, /* Width, in pixels, of space character */
+    fontdata_10x18_Descriptors, /* Character descriptor array */
+    fontdata_10x18, /* Character bitmap array */
+};
+EOF
+fi
