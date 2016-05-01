46ca1b40c5e71f3f55486762a302db52b850b1c5
Stephen Bird <sbird@cyngn.com>
2016-05-01 01:50:49 -0700

    Revert "Menu: add null check for submenu"
    This reverts commit 9342db13f7d6373224be852026896adbbe9eb3fb.

--- menu.c
+++ menu.c
@@ -121,13 +121,11 @@ void menuSelect(uint8_t state, uint32_t duration) {
             mg->menuOpen = 0;
             break;
         case SUBMENU:
-            if (MI->subMenu != NULL) {
-                if (MI->getMenuDef != NULL)
-                    MI->getMenuDef(MI);
-                t = mg;
-                runMenu(MI->subMenu);
-                mg = t;
-            }
+            if (MI->getMenuDef != NULL)
+                MI->getMenuDef(MI);
+            t = mg;
+            runMenu(MI->subMenu);
+            mg = t;
             break;
 
     }
