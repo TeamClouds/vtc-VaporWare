6016b771140dd89186ce08d945d53f67c93824b8
Pat Erley <pat-lkml@erley.org>
2016-05-07 03:20:59 -0700

    new_fw: Add short function for usb printing
    Get rid of the steps you always have to do.  use like printf

--- debug.h
+++ debug.h
@@ -1,8 +1,20 @@
 #ifndef ____DEBUG
 #define ____DEBUG
 #include <USB_VirtualCOM.h>
-
+#include <stdarg.h>
 
 #define D() ;{char __buff[63];siprintf(__buff, "File: %s Line: %i\r\n", __FILE__, __LINE__);USB_VirtualCOM_SendString(__buff);};
 #define W() ;{while(USB_VirtualCOM_GetAvailableSize() == 0){;}};
+
+static inline void writeUsb(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    char buff[63] = {0};
+    vsiprintf(buff, format, args);
+    USB_VirtualCOM_SendString(buff);
+
+    va_end(args);
+}
+
 #endif
